37894047f4549f02c5028380eeadd2ee8020a5dd
Martin K. <martin88.2016@abv.bg>
2025-06-23 23:42:30 +0300

    Sorting

--- Supermarket/Main.cpp
+++ Supermarket/Main.cpp
@@ -1,20 +1,20 @@
 
 
-#include "Utils/Class Helpers/Other/Random.hpp"
+#include "SupermarketSystem.h"
 
 #include <iostream>
 
 int main()
 {
 	
-	my_string str = "TESTSTR ";
-
-	str.push(Random::randomDigit());
-	str.push(Random::randomUpper());
-	str.push(Random::randomDigit());
-	str.push(Random::randomUpper());
-
-	std::cout << str;
+	//my_string str = "TESTSTR ";
+	//
+	//str.push(Random::randomDigit());
+	//str.push(Random::randomUpper());
+	//str.push(Random::randomDigit());
+	//str.push(Random::randomUpper());
+	//
+	//std::cout << str;
 
 }
 
